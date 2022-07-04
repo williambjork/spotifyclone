f023aea3892346b8276c353baed96fcfaed363da
williambjork <william.bjork@gobil.se>
2022-07-04 21:13:45 +0200

    added scrollbar and center

---
+++ components/center.js
@@ -0,0 +1,11 @@
+function Center() {
+    
+    return (
+        <div className="flex flex-grow text-white">
+            <h1> I am center</h1>
+        </div>
+    )
+    
+}
+
+export default Center
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 
 import Head from 'next/head'
 import Image from 'next/image'
+import Center from '../components/Center';
 import Sidebar from '../components/sidebar';
 
 export default function Home() {
   return (
     <>
       <div className="bg-black h-screen overflow-hidden">
-        <main>
+        <main className="flex">
           <Sidebar />
+          <Center />
 
           {/*center */}
 
