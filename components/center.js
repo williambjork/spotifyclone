0221808742125eb95fccd4334eae1d94d5f81117
williambjork <william.bjork@gobil.se>
2022-07-04 23:54:41 +0200

    fixed tailwindcss plugin issue

--- components/center.js
+++ components/center.js
@@ -1,8 +1,17 @@
+import { useSession } from "next-auth/react";
+
 function Center() {
     
+    const { data: session } = useSession();
+
     return (
         <div className="flex flex-grow text-white">
             <h1> I am center</h1>
+            <header>
+                <div>
+                    <img src={session?.user.image} alt="" />
+                </div>
+            </header>
         </div>
     )
     
