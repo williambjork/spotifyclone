984e5bf12513666c3c3f547bea4d030fe8b7cae6
williambjork <william.bjork@gobil.se>
2022-07-15 12:28:38 +0200

    Fixed hydration issue in sidebar

--- components/center.js
+++ components/center.js
@@ -25,7 +25,7 @@ function Center() {
     }, []);
     
     return (
-        <>
+        
         <div className="flex-grow">                   
             <header className="absolute top-5 right-8">
                 <div className="flex items-center bg-black text-white 
@@ -33,18 +33,17 @@ function Center() {
                                 rounded-full cursor-pointer p-1 pr-2">                   
                     <img className="rounded-full w-10 h-10" src={session?.user.image} alt="" />
                     <h2>{session?.user.name}</h2>
-                    <ChevronDownIcon className="h-5 w-5 "/>
-                </div>
+                    <ChevronDownIcon className="h-5 w-5 "/>                
+                </div>             
             </header>
         
 
             <section className={`flex items-end space-x-7 bg-gradient-to-b 
                                 to-black ${color}-500 h-80 text-white p-8 w-full`}>
-         
-            </section>
+             </section>
         </div>
-    </>
-    )
+    
+    );
     
 }
 
