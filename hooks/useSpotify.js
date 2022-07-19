a52f6db542e35990d4192ae2950d53301fce060b
williambjork <william.bjork@gobil.se>
2022-07-19 15:06:14 +0200

    Started playlist import - prettyfied

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,18 +1,26 @@
-import { useEffect } from "react";
-import { signIn, useSession } from "next-auth/react";
+import { useEffect } from 'react'
+import { signIn, useSession } from 'next-auth/react'
+import SpotifyWebApi from 'spotify-web-api-node'
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+})
 
 function useSpotify() {
-    const { data: session, status } = useSession();
-    
-    useEffect(() => {
-        if (session) {
-            if (session.error === "RefreshAccessTokenError") {
-                signIn();
-            }
-        }
-    }, [session]);
+  const { data: session, status } = useSession()
 
-    return null
-} 
+  useEffect(() => {
+    if (session) {
+      if (session.error === 'RefreshAccessTokenError') {
+        signIn()
+      }
+
+      spotifyApi.setAccessToken(session.user.setAccessToken)
+    }
+  }, [session])
+
+  return spotifyApi
+}
 
 export default useSpotify
