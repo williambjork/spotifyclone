c925a8ea865706922c11059f9df7e81e1466338b
williambjork <william.bjork@gobil.se>
2022-08-25 09:59:30 +0200

    mapping songs

--- components/songs.js
+++ components/songs.js
@@ -1,7 +1,16 @@
+import { useRecoilValue } from "recoil"
+import { playlistState } from "../atoms/playlistAtom";
+
 function Songs() {
+    const playlist = useRecoilValue(playlistState);
+
   return (
-    <div>Songs</div>
-  )
+    <div className="text-white">
+        {playlist?.tracks.items.map((track) => (
+            <div>{track.track.name} </div>
+        ))}
+    </div>
+        )
 }
 
 export default Songs
